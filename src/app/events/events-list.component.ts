d86d9c7fa0b5f05d013aeccf22cffdabc2d3bb40
Amit <amit.k15@outlook.com>
2018-08-19 00:50:34 +0530

    Using External Templates

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,29 +2,13 @@ import { Component } from '@angular/core'
 
 @Component({
     selector:'events-list',
-    template:`
-    <div>
-        <h1>Upcoming Angular Events</h1>
-        <hr/> 
-        <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <div>Price: \${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span>&nbsp;</span>
-                <span>{{event.location.city}}, {{event.location.country}}</span>
-            </div>
-        </div>
-    </div>
-    `
+    templateUrl:'./events-list.component.html' //relative to component
 })
 
 export class EventListComponent{
     event = {
         id:1,
-        name: 'Angular Conenct',
+        name: 'Angular Connect',
         date:'9/26/2036',
         time:'10:00 am',
         price:599.99,
